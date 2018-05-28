8426cbed3702f1f91f0be33c41dbe95e310aad2c
zjubird <pz@pdeMacBook-Pro.local>
2018-05-29 07:03:32 +0800

    初步具备网络能力

--- wiki.go
+++ wiki.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"log"
 )
 
 type Page struct{
@@ -24,12 +26,21 @@ func loadPage(title string) (*Page, error){
 	return &Page{Title: title, Body:body}, nil
 }
 
+func viewHandler(rsp http.ResponseWriter, r *http.Request){
+	//rsp is repsonse, r is request
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(rsp, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 
 func main(){
-	p1 := &Page{Title :"TestPage", Body:[]byte("This is a sample Page")}
-	p1.save()
-
-	p2, _ := loadPage("TestPage")
-
-	fmt.Println(string(p2.Body))
+//	p1 := &Page{Title :"TestPage", Body:[]byte("This is a sample Page")}
+//	p1.save()
+//
+//	p2, _ := loadPage("TestPage")
+//
+//	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler) //handle all requests with view to handler viewHandler
+	log.Fatal(http.ListenAndServe(":8080", nil))//listen on port 8080
 }
