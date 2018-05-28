d07568ec3cc2b9b662dead233998f70140c084fe
zjubird <pz@pdeMacBook-Pro.local>
2018-05-29 07:25:34 +0800

    add view && edit, learn http template

--- wiki.go
+++ wiki.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"log"
+	"html/template"
 )
 
 type Page struct{
@@ -26,13 +27,34 @@ func loadPage(title string) (*Page, error){
 	return &Page{Title: title, Body:body}, nil
 }
 
+func renderTemplate(w http.ResponseWriter, templ string, p *Page){
+	t, _ := template.ParseFiles(templ + ".html")
+	t.Execute(w, p)
+}
 func viewHandler(rsp http.ResponseWriter, r *http.Request){
 	//rsp is repsonse, r is request
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
-	fmt.Fprintf(rsp, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	renderTemplate(rsp,"view",p)
+	//t, _ := template.ParseFiles("view.html")
+	//t.Execute(rsp, p)
+	//fmt.Fprintf(rsp, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+
+func editHandler(rsp http.ResponseWriter, r *http.Request){
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil{
+		p = &Page{Title: title}
+	}
+	renderTemplate(rsp, "edit", p)
+//	t, _ := template.ParseFiles("edit.html")
+//	t.Excecute(rsp, p)
+}
+
+func saveHandler(rsp http.ResponseWriter, req *http.Request){
+}
 
 func main(){
 //	p1 := &Page{Title :"TestPage", Body:[]byte("This is a sample Page")}
@@ -42,5 +64,8 @@ func main(){
 //
 //	fmt.Println(string(p2.Body))
 	http.HandleFunc("/view/", viewHandler) //handle all requests with view to handler viewHandler
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))//listen on port 8080
+	fmt.Println("...")
 }
