25f3b461e2dd7007bbf2ed629865165a9c974553
zjubird <pz@pdeMacBook-Pro.local>
2018-05-29 07:43:07 +0800

    template and save

--- wiki.go
+++ wiki.go
@@ -13,6 +13,8 @@ type Page struct{
 	Body  []byte
 }
 
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
 func (p *Page) save() error{
 	filename := p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
@@ -28,19 +30,26 @@ func loadPage(title string) (*Page, error){
 }
 
 func renderTemplate(w http.ResponseWriter, templ string, p *Page){
-	t, _ := template.ParseFiles(templ + ".html")
-	t.Execute(w, p)
+	err := templates.ExecuteTemplate(w, templ + ".html", p)
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
+
 func viewHandler(rsp http.ResponseWriter, r *http.Request){
 	//rsp is repsonse, r is request
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
+	p, err := loadPage(title)
+	if err != nil{
+		http.Redirect(rsp, r, "/edit/"+title, http.StatusFound)
+		return
+	}
 	renderTemplate(rsp,"view",p)
 	//t, _ := template.ParseFiles("view.html")
 	//t.Execute(rsp, p)
 	//fmt.Fprintf(rsp, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
-
 
 func editHandler(rsp http.ResponseWriter, r *http.Request){
 	title := r.URL.Path[len("/edit/"):]
@@ -54,6 +63,15 @@ func editHandler(rsp http.ResponseWriter, r *http.Request){
 }
 
 func saveHandler(rsp http.ResponseWriter, req *http.Request){
+	title := req.URL.Path[len("/save/"):]
+	body := req.FormValue("body")
+	p := &Page{Title:title, Body:[]byte(body)}
+	err := p.save()
+	if err != nil{
+		http.Error(rsp, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(rsp,req,"/view/"+title, http.StatusFound)
 }
 
 func main(){
